87e7086d515cca32c5e737d3b2afc8557e00ce70
Stephanie Viveros <sviveros05@gmail.com>
2024-11-16 00:26:46 -0600

    Remove footer from bottom of site and clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./pages/Nav";
-import Footer from "./pages/Footer";
-
 const App = () => {
   return (
     <>
       <Nav />
       <Outlet />
-      {/* <Footer /> */}
     </>
   );
 };
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,47 +1,6 @@
-import { Link } from "react-router-dom";
-import {
-  nav,
-  contactInfo,
-} from "../constants";
-
-const siteMap = nav.map(({ path, label }) => (
-  <Link key={`footer-${label}`} className="footer-link" to={path}>
-    {label}
-  </Link>
-));
-
-const contact = contactInfo.map((info) => {
-  const [key, value] = Object.entries(info)[0];
-  return (
-    <a
-      key={key}
-      aria-label={`${key} ${value}`}
-      className="footer-link"
-      href={value}
-    >
-      {key}
-    </a>
-  );
-});
-
 const Footer = () => {
   return (
-    <footer className="text-[#e8e6e6] absolute inset-x-0 bottom-0 text-center text-sm sm:text-xs">
-      {/* <div className="m-10">
-        <i className="fas fa-code" />
-      </div> */}
-      {/* <section className="hidden md:flex justify-center gap-x-40 text-center">
-        <div>
-          <label className="block">Contact</label>
-          {contact}
-        </div>
-
-        <div className="">
-          <label className="block">Pages</label>
-          {siteMap}
-        </div>
-      </section> */}
-
+    <footer className="absolute inset-x-0 text-center text-sm sm:text-xs">
       <section className="mt-10">
         <div>
           <a
@@ -72,13 +31,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// •	Footer Headings:
-// •	Mobile: text-base
-// •	Tablet: text-lg
-// •	Desktop: text-lg
-
-// •	Footer Links and Text:
-// •	Mobile: text-xs
-// •	Tablet: text-sm
-// •	Desktop: text-sm
